a461a437abb77ea5ffd378bf8ae63458d6f8040c
elcomendante <karol@finsane.com>
2022-07-04 11:51:58 +0100

    script-ok

--- script/SimpleToken.s.sol
+++ script/SimpleToken.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/SimpleToken.sol";
 
 contract SimpleTokenScript is Script {
-    function setUp() public {}
+    // function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        SimpleToken simpleToken = new SimpleToken();
+        vm.stopBroadcast();
     }
 }
--- test/SimpleToken.t.sol
+++ test/SimpleToken.t.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "forge-std/Test.sol";
 import "../src/SimpleToken.sol";
+
 contract SimpleTokenTest is Test {
     // Target contract
     SimpleToken simpleToken;
